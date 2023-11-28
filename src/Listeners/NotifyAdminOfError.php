0e85d4ccac6720283c970460ba1e883481218aa0
Mayonobe Hugo <hugomayonobe@gmail.com>
2023-11-28 17:00:10 +0100

    verify error context is not empty

--- src/Listeners/NotifyAdminOfError.php
+++ src/Listeners/NotifyAdminOfError.php
@@ -5,6 +5,7 @@ namespace Hugomyb\ErrorMailer\Listeners;
 
 
 use Hugomyb\ErrorMailer\Notifications\ErrorOccurred;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 
@@ -28,14 +29,23 @@ class NotifyAdminOfError
      */
     public function handle($event)
     {
-        if (!in_array(env('APP_ENV'), config('error-mailer.disabledOn')) ) {
-            if (config()->has('error-mailer.email.recipient')) {
-                $recipient = config('error-mailer.email.recipient');
-            } else {
-                $recipient = 'destinataire@example.com';
-            }
+        if (!in_array(env('APP_ENV'), config('error-mailer.disabledOn'))) {
 
-            Mail::to($recipient)->send(new ErrorOccurred($event->context['exception']));
+            $recipient = config()->has('error-mailer.email.recipient')
+                ? config('error-mailer.email.recipient')
+                : 'destinataire@example.com';
+
+            $errorHash = md5($event->context['exception']->getMessage() . $event->context['exception']->getFile());
+
+            $cacheKey = 'error_mailer_' . $errorHash;
+            $coolDownPeriod = 15;
+
+            if (!Cache::has($cacheKey)) {
+                if (isset($event->context['exception'])) {
+                    Mail::to($recipient)->send(new ErrorOccurred($event->context['exception']));
+                    Cache::put($cacheKey, true, now()->addMinutes($coolDownPeriod));
+                }
+            }
         }
     }
 }
